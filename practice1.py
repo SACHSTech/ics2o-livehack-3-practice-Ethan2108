8d6e4722b4dfd2e60d0181356bc08f13595b3ed3
Ethan Au <ethan.au23@ycdsbk12.ca>
2021-01-06 15:51:02 +0000

    complete

--- practice1.py
+++ practice1.py
@@ -1,1 +1,24 @@
+"""
+-------------------------------------------------------------------------------
+Name:		practice1.py
+Purpose: To create a program that takes in the starting price, and ending price based on the Canadian to U.S dollar exchange rate, and then outputs a conversion table of Canadian to U.S Dollars with increments of $10.
 
+Author:	 Au.E
+
+Created:	01/06/2021
+-------------------------------------------------------------------------------
+"""
+
+
+# get the exchange rate and start and end of table
+exchange_rate = 0.75
+start_value  = int(input("Enter the starting value: "))
+end_value  = int(input("Enter the end value: "))
+
+# table header
+print("Can Price  --> US Price")
+
+# output table
+for cdn_amount in range (start_value, end_value+1, 10):
+  usd_amount = cdn_amount * exchange_rate
+  print(str(cdn_amount) + " --> " + str(usd_amount))
--- practice2.py
+++ practice2.py
@@ -1,1 +1,36 @@
+"""
+-------------------------------------------------------------------------------
+Name:		practice2.py
+Purpose: To determine if fees need to be paid at customs when reentering Canada based on the monetary value of goods purchased during time spent abroad
+
+Author:	Au.E
+
+Created:	01/06/2021
+-------------------------------------------------------------------------------
+"""
+
+total = 0
+days_count = 0
+done = False
+
+while not done:
+  daily_amount = float(input("Enter a daily spent amount (-1 to stop): "))
+
+  if daily_amount == -1:
+    done = True
+  else:
+    total = total + daily_amount
+    days_count = days_count + 1
+
+# output number of days and total spent
+print("Total days travelled: " + str(days_count))
+print("Total amount spent: $" + str(total))
+
+# check if they've gone over the limit
+limit = 250 * days_count
+if total > limit:
+  fee = total * 0.13
+  print("You owe a fee of $" + str(round(fee, 2)))
+else:
+  print("You do not owe a fee!!!")
 
